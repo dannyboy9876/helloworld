25b7b86eb72c3ae626973ba7868284498c19a961
jim <jamespressler1@gmail.com>
2015-10-27 12:18:56 -0400

    dgdgd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Helfffffffddddlo ggggggggg!");
+        System.out.println("ggggggggg ggggggggg!");
     }
 }
